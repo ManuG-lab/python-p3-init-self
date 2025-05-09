efa0246f7c48bfcd929771ac2fcd7c4cd64e6871
Emmanuel Gitau <gmannu2055@gmail.com>
2025-05-09 11:09:48 +0300

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+
+Snoopy = Dog("Snoopy")
+print(Snoopy.name, Snoopy.breed)
